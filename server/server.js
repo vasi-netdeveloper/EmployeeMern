308284a6af94674572e95fca62c32be5cb48efcb
Vasi <Vasi.Vohra@appech.com>
2023-12-23 20:06:59 +0530

    added-server-side-code

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env"});
+const port = process.env.port || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+const dbo = require("./db/conn");
+app.listen(port, () => {
+    dbo.connectToServer(function (err){
+        if(err) console.error(err);
+    })
+})
